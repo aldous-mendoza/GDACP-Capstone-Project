121c1d927e198021a2012ac97dac8a0770c24fc0
aldous-mendoza <126262254+aldous-mendoza@users.noreply.github.com>
2024-01-31 10:39:42 +0800

    2_Data_Exploration.sql

--- 2_Data_Exploration.sql
+++ 2_Data_Exploration.sql
@@ -1,84 +1,93 @@
--- Data analysis
+-- Data Exploration
 
--- Number of riders per bike type 
+-- Summarizing the column names and their corresponding data types
 
-SELECT member_casual, rideable_type, COUNT(*) AS number_of_trips
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY member_casual, rideable_type
-ORDER BY member_casual, number_of_trips;
+SELECT  column_name, data_type
+FROM `2022_bike_tripdata`.INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'merged_bike_tripdata';
 
--- Number of trips per month
+-- Checking each column for the number of their NULL values
 
-SELECT month, member_casual, COUNT(ride_id) AS number_of_trips
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY month, member_casual;
+SELECT 
+  COUNT(*) - COUNT(ride_id) ride_id,
+  COUNT(*) - COUNT(rideable_type) rideable_type,
+  COUNT(*) - COUNT(started_at) started_at,
+  COUNT(*) - COUNT(ended_at) ended_at,
+  COUNT(*) - COUNT(start_station_name) start_station_name,
+  COUNT(*) - COUNT(start_station_id) start_station_id,
+  COUNT(*) - COUNT(end_station_name) end_station_name,
+  COUNT(*) - COUNT(end_station_id) end_station_id,
+  COUNT(*) - COUNT(start_lat) start_lat,
+  COUNT(*) - COUNT(start_lng) start_lng,
+  COUNT(*) - COUNT(end_lat) end_lat,
+  COUNT(*) - COUNT(end_lng) end_lng,
+  COUNT(*) - COUNT(member_casual) member_casual
+FROM `2022_bike_tripdata.merged_bike_tripdata`;
 
--- Number of trips per day of the week
+-- Checking for duplicate rows
 
-SELECT day_of_week, member_casual, COUNT(ride_id) AS number_of_trips 
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY day_of_week, member_casual;
+SELECT COUNT(ride_id) - COUNT(DISTINCT(ride_id)) AS duplicate_rows_number
+FROM `2022_bike_tripdata.merged_bike_tripdata`;
 
--- Number of trips per hour
+-- ride_id: checking for invalid values for the primary key 
 
-SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual,
-  COUNT(ride_id) AS number_of_trips
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY hour_of_day, member_casual
-ORDER BY hour_of_day;
+SELECT LENGTH(ride_id) AS length_ride_id, COUNT(ride_id) AS number_of_rows
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+GROUP BY length_ride_id;
 
--- Average ride length per month
+-- rideable_type: enumerating the types of bike
 
-SELECT month, member_casual, AVG(ride_length) AS avg_ride_length
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY month, member_casual;
+SELECT DISTINCT(rideable_type), COUNT(*) number_of_bikes
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+GROUP BY rideable_type;
 
--- Average ride length per day of week
+-- started_at and ended_at: checking for invalid trip durations 
 
-SELECT day_of_week, member_casual, AVG(ride_length) AS avg_ride_length
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY day_of_week, member_casual;
+#Preview Time format
 
--- Average ride_length per hour
+SELECT started_at, ended_at
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+LIMIT 10;
+#Time format: YYY-MM-DD hh:mm:ss UTC)
 
-SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual, AVG(ride_length) AS avg_ride_length
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY hour_of_day, member_casual;
+#Counting < 1 minute
 
--- Starting station locations
+SELECT COUNT(*) AS less_than_1_minute
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+WHERE TIMESTAMP_DIFF(ended_at, started_at, SECOND) <= 60;
+#Number of rows with less than 1 minute = 12283
 
-SELECT start_station_name, member_casual,
-  AVG(start_lat) AS start_lat, AVG(start_lng) AS start_lng,
-  COUNT(ride_id) AS number_of_trips
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY start_station_name, member_casual;
+#Counting > 24 hours 
 
--- Ending station locations
+SELECT COUNT(*) AS greater_than_24_hours
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+WHERE TIMESTAMP_DIFF(ended_at, started_at, MINUTE) >= (24 * 60);
+#Number of rows with greater than 24 hours = 5360
 
-SELECT end_station_name, member_casual,
-  AVG(end_lat) AS end_lat, AVG(end_lng) AS end_lng,
-  COUNT(ride_id) AS number_of_trips
-FROM `2022_bike_tripdata.cleaned_merged_data`
-GROUP BY end_station_name, member_casual;
+-- start_station_name and start_station_id: Double checking for missing rows
 
--- Top 5 starting station locations 
+SELECT COUNT(ride_id) AS null_start_station_rows
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+WHERE start_station_name IS NULL OR start_station_id IS NULL;
+#Number of missing rows = 833064
 
-SELECT start_station_name, 
-  COUNT(ride_id) AS number_of_trips
-FROM `2022_bike_tripdata.cleaned_merged_data`
-WHERE member_casual = 'member'
-GROUP BY start_station_name
-ORDER BY number_of_trips DESC
-LIMIT 5
+-- end_station_name and end_station_id: Double checking for missing rows
 
--- Top 5 ending station locations
+SELECT COUNT(ride_id) AS null_end_station_rows
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+WHERE end_station_name IS NULL OR end_station_id IS NULL;
+#Number of missing rows = 892742
 
-SELECT end_station_name, 
-  COUNT(ride_id) AS number_of_trips
-FROM `2022_bike_tripdata.cleaned_merged_data`
-WHERE member_casual = 'member'
-GROUP BY end_station_name
-ORDER BY number_of_trips DESC
-LIMIT 5
+-- end_lat and end_lng: Double checking for missing rows
 
+SELECT COUNT(ride_id) AS null_end_position
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+WHERE end_lat IS NULL OR end_lng IS NULL;
+#Number of missing rows = 5858
+
+-- member_casual: Counting the number of consumers for each segment
+
+SELECT DISTINCT member_casual, COUNT(member_casual) AS number_of_consumers
+FROM `2022_bike_tripdata.merged_bike_tripdata`
+GROUP BY member_casual;
 
